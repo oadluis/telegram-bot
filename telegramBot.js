a271e99807f60ca21934969d60bb7cab94a8c8d4
oandrelb <andre.barbosa.1390@gmail.com>
2025-04-29 18:26:22 -0300

    aplicando o .env e modificando o .gitignore

---
+++ telegramBot.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const TelegramBot = require("node-telegram-bot-api");
+const token = process.env.BOT_TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+bot.on("message", (msg) => {
+  const chatId = msg.chat.id;
+  const messageText = msg.text;
+
+  if (messageText === "/start") {
+    bot.sendMessage(chatId, "Bem vindo ao nacaovivabot!");
+  }
+});
