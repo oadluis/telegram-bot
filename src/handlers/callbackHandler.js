7182a9c18f59f0314032f62e05e1f1b7a6692e59
oandrelb <andre.barbosa.1390@gmail.com>
2025-05-02 20:22:31 -0300

    refatorando o codigo do arquivo index.js para apenas inicializar o bot e nada mais

---
+++ src/handlers/callbackHandler.js
@@ -0,0 +1,43 @@
+const sendMainMenu = require('../menus/mainMenu');
+const sendTechMenu = require('../menus/sections/techMenu');
+const sendScienceMenu = require('../menus/sections/scienceMenu');
+const fetchNews = require('../helpers/fetchNews');
+const calculateDates = require('../helpers/calculateDates');
+
+module.exports = (bot) => {
+  bot.on('callback_query', (query) => {
+    console.log(query);
+    const chatId = query.message.chat.id;
+    const data = query.data;
+
+    switch (true) {
+      case data === 'section_tech':
+        sendTechMenu(bot, chatId);
+        break;
+
+      case data === 'section_science':
+        sendScienceMenu(bot, chatId);
+        break;
+
+      case data === 'back_to_main':
+        sendMainMenu(bot, chatId);
+        break;
+
+      case data.startsWith('tech_'):
+        const topic = data.split('_')[1];
+        const { fromDate, toDate } = calculateDates('hoje');
+        fetchNews(
+          bot,
+          process.env.NEWS_API_KEY,
+          chatId,
+          topic,
+          fromDate,
+          toDate
+        );
+        break;
+
+      default:
+        console.log(`Callback não reconhecido: ${data}`);
+    }
+  });
+};
