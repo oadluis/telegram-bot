e966593910f571b68f0d61ed70d98e638979dbc1
oandrelb <andre.barbosa.1390@gmail.com>
2025-05-02 21:31:43 -0300

    atualizando o callbackHandler para que ele abranja as novas sessões

--- src/handlers/callbackHandler.js
+++ src/handlers/callbackHandler.js
@@ -5,12 +5,14 @@ const sendMainMenu = require('../menus/mainMenu');
 const sendScienceMenu = require('../menus/sections/scienceMenu');
 const sendSportsMenu = require('../menus/sections/sportsMenu');
 const sendTechMenu = require('../menus/sections/techMenu');
+const sendHealthMenu = require('../menus/sections/healthMenu');
+const sendEntertainmentMenu = require('../menus/sections/entertainmentMenu');
+const sendEnvironmentMenu = require('../menus/sections/environmentMenu');
 const { topicMap, messages } = require('../utils/constants');
 
 module.exports = (bot) => {
   bot.on('callback_query', async (query) => {
     try {
-      console.log(query);
       const chatId = query.message.chat.id;
       const data = query.data;
 
@@ -27,13 +29,30 @@ module.exports = (bot) => {
           sendSportsMenu(bot, chatId);
           break;
 
+        case data === 'section_health':
+          sendHealthMenu(bot, chatId);
+          break;
+
+        case data === 'section_entertainment':
+          sendEntertainmentMenu(bot, chatId);
+          break;
+
+        case data === 'section_environment':
+          sendEnvironmentMenu(bot, chatId);
+          break;
+
         case data === 'back_to_main':
           sendMainMenu(bot, chatId);
           break;
 
         case data.startsWith('tech_'):
+        case data.startsWith('science_'):
+        case data.startsWith('sports_'):
+        case data.startsWith('health_'):
+        case data.startsWith('entertainment_'):
+        case data.startsWith('environment_'):
           try {
-            const topic = data.split('_')[1];
+            const [section, topic] = data.split('_');
             const { fromDate, toDate } = calculateDates('hoje');
             const articles = await fetchNews(
               bot,
@@ -44,7 +63,7 @@ module.exports = (bot) => {
               toDate
             );
 
-            if ((!Array, isArray(articles) || articles.length === 0)) {
+            if (!Array.isArray(articles) || articles.length === 0) {
               bot.sendMessage(chatId, messages.noNews);
               break;
             }
@@ -64,16 +83,20 @@ module.exports = (bot) => {
               },
             });
           } catch (error) {
-            console.error('Erro ao calcular datas:', error);
+            console.error('Erro ao buscar notícias:', error);
             bot.sendMessage(
               chatId,
-              'Ocorreu um erro ao calcular as datas. Por favor, tente novamente.'
+              'Ocorreu um erro ao buscar notícias. Por favor, tente novamente.'
             );
           }
           break;
 
         default:
           console.log(`Callback não reconhecido: ${data}`);
+          bot.sendMessage(
+            chatId,
+            'Opção não reconhecida. Por favor, tente novamente.'
+          );
       }
     } catch (error) {
       console.error('Erro no callback handler:', error);
